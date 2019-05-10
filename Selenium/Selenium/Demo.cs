b3538937745622088907f1e64e9be1796d75ae6b
testacademy <47723487+testacademy@users.noreply.github.com>
2019-05-10 23:17:20 +0300

    Adding demo Selenium

---
+++ Selenium/Selenium/Demo.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium
+{
+    class Demo
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver("C:\\Users\\rutapo\\Desktop\\chromedriver_win32");
+        }
+
+        string[] usernames = { "rutaadmin", "admin" };
+        string[] passwords = { "wavedms110rpadmin", "admin" };
+
+        public void login(string user, string password)
+        {
+            driver.FindElement(By.XPath("//*[@id='Username']")).SendKeys(user);
+            driver.FindElement(By.XPath("//*[@id='Password']")).SendKeys(password);           
+        }
+       
+        [Test]
+        public void test()
+        {
+            driver.Url = "http://dms110:85";
+            login(usernames[0], passwords[0]);
+            driver.FindElement(By.XPath("//*[@id='loginButton']")).Click();
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            //driver.Close();
+        }
+    }
+}
